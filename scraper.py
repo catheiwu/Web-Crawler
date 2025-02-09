1694a822d1f030f7753a09343595120831b3c917
dmaseque <dmaseque@uci.edu>
2025-02-09 13:49:52 -0800

    Implement draft of is_valid function

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import re
 from urllib.parse import urlparse
 
+# only crawl the following URLS and paths (valid domains)
+VALID_DOMAINS = [".ics.uci.edu", ".cs.uci.edu", ".informatics.uci.edu", ".stat.uci.edu"]
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -9,12 +12,13 @@ def extract_next_links(url, resp):
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.  
     # resp.error: when status is not 200, you can check the error here, if needed.
     # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+
     return list()
 
 def is_valid(url):
@@ -22,9 +26,24 @@ def is_valid(url):
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
+
         parsed = urlparse(url)
+
+        # only vaid if scheme is http or https
         if parsed.scheme not in set(["http", "https"]):
             return False
+        
+        # defragment URL (removing the fragment part)
+        if parsed.fragment:
+            return False
+        
+        # only valid if domain is in VALID_DOMAINS
+        # parse.netloc.lower() returns the domain and port
+        domain = parsed.netloc.lower()
+        if not any(re.search(valid_domain, domain) for valid_domain in VALID_DOMAINS):
+            return False
+        
+        # not valid if not html link
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
