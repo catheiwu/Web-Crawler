b45b420de8ae26b099d0ae05119a8098a6ce9edb
Cathy <cathyw8@uci.edu>
2025-02-09 15:20:49 -0800

    initial run

--- scraper.py
+++ scraper.py
@@ -1,5 +1,10 @@
 import re
 from urllib.parse import urlparse
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+# only crawl the following URLS and paths (valid domains)
+VALID_DOMAINS = [".ics.uci.edu", ".cs.uci.edu", ".informatics.uci.edu", ".stat.uci.edu"]
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,16 +20,50 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    try:
+        soup = BeautifulSoup(resp.raw_response.content, features="lxml") # raw_response.content gives you the webpage html content, pass additional argument of parser specified to lxml
+        links = set() # create empty set to store UNIQUE URLs found on page
+        
+        # urlparse breaks down URL into its compoentns (scheme, netloc, path, query, etc.)
+        base_url = urlparse(url).scheme + "://" + urlparse(url).netloc # netloc aka authority
+
+        for anchor in soup.find_all("a", href=True): # find all anchor tags <a> that define href attributes (hyperlinks)
+            # transform relative to absolute URLs
+            absolute_url = urljoin(base_url, anchor["href"].strip()) # constructs full url by joining base w/ whatever hyperlinks are found on page
+            links.add(absolute_url)
+
+        return list(links) # converts set (uniqueness) to list (return value)
+    
+    except Exception as e:
+        print(f"Error parsing {url}: {e}") # parsing fails
+        return [] # returns empty list
+    # return list()
+
+def defragment(url): # preserves the original url w/o fragment
+    parsed = urlparse(url)
+    return parsed.scheme + "://" + parsed.netloc + parsed.path + ("?" + parsed.query if parsed.query else"")
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
+        
+        # defragment URL (removing the fragment part)
+        url = defragment(url)
         parsed = urlparse(url)
+
+        # only vaid if scheme is http or https
         if parsed.scheme not in set(["http", "https"]):
             return False
+        
+        # only valid if domain is in VALID_DOMAINS
+        # parse.netloc.lower() returns the domain and port
+        domain = parsed.netloc.lower()
+        if not any(re.search(valid_domain, domain) for valid_domain in VALID_DOMAINS):
+            return False
+        
+        # not valid if url does not point to a webpage
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
